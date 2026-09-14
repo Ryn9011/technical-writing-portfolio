27abc5cd0b26fec0ca109a1fdb04b9af49fef3ff
Ryn9011 <ryan.jennings83@gmail.com>
2026-09-14 15:28:42 +1200

    REmoved footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       <body className="flex min-h-full flex-col bg-white text-slate-900">
         <SiteHeader />
         <main className="flex-1">{children}</main>
-        <SiteFooter />
+        {/* <SiteFooter /> */}
       </body>
     </html>
   );
